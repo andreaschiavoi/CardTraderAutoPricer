ad481830d386d464ef6a8aec9630f6e8eb31b1e3
andrea.schiavoi <andrea.schiavoi@omnys.com>
2025-08-04 07:55:00 +0200

    spring project from spring initializr

---
+++ src/main/java/com/marugami/ct_autopricer/CtAutopricerApplication.java
@@ -0,0 +1,13 @@
+package com.marugami.ct_autopricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtAutopricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtAutopricerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marugami/ct_autopricer/CtAutopricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marugami.ct_autopricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtAutopricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
